364040de22caabdfffa93706b0e629057b3ee79f
davidcoats <david@getsafetycone.com>
2023-09-17 11:58:00 -0700

    Initial commit.

---
+++ source/R5T.L0064.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.L0064.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.L0064/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0064
+{
+	/// <summary>
+	/// .NET pack (dotnet pack) library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
